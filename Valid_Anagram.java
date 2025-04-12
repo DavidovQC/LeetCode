aff6d8b9b2e1ad053a922269de23044bdfc8ba19
Nathan Davidov <NathanDavidov9@gmail.com>
2025-04-12 17:58:54 -0400

    Valid_Anagram

---
+++ Valid_Anagram.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null){
+            if(s == null && t == null){
+                return true;
+            }
+            else return false;
+        }
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        else{
+           int[] arr = new int[26];
+           for(int i = 0; i < s.length(); i++){
+                arr[s.charAt(i) - 'a']++;
+                arr[t.charAt(i) - 'a']--;
+           }
+
+            for(int i = 0; i < 26; i++){
+                if(arr[i] != 0){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
